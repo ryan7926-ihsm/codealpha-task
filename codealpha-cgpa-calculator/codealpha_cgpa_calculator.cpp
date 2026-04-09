077d1d2f640285e231aea333d118f93d07cbf353
Muhammad Ryan <m.reee7926@gmail.com>
2026-04-09 22:49:17 +0500

    Update codealpha_cgpa_calculator.cpp

--- codealpha-cgpa-calculator/codealpha_cgpa_calculator.cpp
+++ codealpha-cgpa-calculator/codealpha_cgpa_calculator.cpp
@@ -1,135 +1,65 @@
 #include <iostream>
 using namespace std;
 
-const int SIZE = 9;
-
-
-void printGrid(int grid[SIZE][SIZE])		// Function to print the Sudoku grid
-{
-    cout << "\nSolved Sudoku:\n";
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
-            cout << grid[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-
-bool isRowSafe(int grid[SIZE][SIZE], int row, int num)		// Check if number is safe in row
-{
-    for (int col = 0; col < SIZE; col++)
-    {
-        if (grid[row][col] == num)
-            return false;
-    }
-    return true;
-}
-
-
-bool isColSafe(int grid[SIZE][SIZE], int col, int num)		// Check if number is safe in column
-{
-    for (int row = 0; row < SIZE; row++)
-    {
-        if (grid[row][col] == num)
-            return false;
-    }
-    return true;
-}
-
-
-bool isBoxSafe(int grid[SIZE][SIZE], int startRow, int startCol, int num)		// Check if number is safe in 3x3 subgrid
-{
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if (grid[startRow + i][startCol + j] == num)
-                return false;
-        }
-    }
-    return true;
-}
-
-
-bool isSafe(int grid[SIZE][SIZE], int row, int col, int num)		// Check if it's safe to place number
-{
-    return isRowSafe(grid, row, num) &&
-           isColSafe(grid, col, num) &&
-           isBoxSafe(grid, row - row % 3, col - col % 3, num);
-}
-
-
-bool solveSudoku(int grid[SIZE][SIZE])			// Main backtracking function
-{
-    int row = -1, col = -1;
-    bool isEmpty = false;
-
-    // Find an empty cell (represented by 0)
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
-            if (grid[i][j] == 0)
-            {
-                row = i;
-                col = j;
-                isEmpty = true;
-                break;
-            }
-        }
-        if (isEmpty)
-            break;
-    }
-
-    // If no empty cell found, Sudoku is solved
-    if (!isEmpty)
-        return true;
-
-    // Try numbers 1 to 9
-    for (int num = 1; num <= 9; num++)
-    {
-        if (isSafe(grid, row, col, num))
-        {
-            grid[row][col] = num;  // Place number
-
-            if (solveSudoku(grid)) // Recursive call
-                return true;
-
-            grid[row][col] = 0;    // Backtrack
-        }
-    }
-
-    return false; // This trigger backtracking
-}
-
 int main()
 {
-    int grid[SIZE][SIZE];
+    int numCourses;
 
-    cout << "Enter Sudoku (use 0 for empty cells):\n";
+    // Take number of courses
+    cout << "Enter number of courses: ";
+    cin >> numCourses;
 
-    // Input grid
-    for (int i = 0; i < SIZE; i++)
+    float grade[numCourses];
+    float credit[numCourses];
+
+    float totalCredits = 0;
+    float totalGradePoints = 0;
+
+    // Input grade and credit hours for each course
+    for (int i = 0; i < numCourses; i++)
     {
-    	cout<<"For Row number "<<i+1<<": "<<endl;
-        for (int j = 0; j < SIZE; j++)
-        {
-            cin >> grid[i][j];
-        }
+        cout << "\nCourse " << i + 1 << endl;
+
+        cout << "Enter grade point: ";
+        cin >> grade[i];
+
+        cout << "Enter credit hours: ";
+        cin >> credit[i];
+
+        // Calculate total grade points
+        totalGradePoints += grade[i] * credit[i];
+        totalCredits += credit[i];
     }
 
-    // Solve Sudoku
-    if (solveSudoku(grid))
+    // Calculate GPA
+    float gpa = totalGradePoints / totalCredits;
+
+    // Display individual course details
+    cout << "\n--- Course Details ---" << endl;
+    for (int i = 0; i < numCourses; i++)
     {
-        printGrid(grid);
+        cout << "Course " << i + 1 
+             << " | Grade: " << grade[i] 
+             << " | Credit Hours: " << credit[i] << endl;
     }
-    else
-    {
-        cout << "No solution exists!\n";
-    }
+
+    // Display GPA
+    cout << "\nSemester GPA: " << gpa << endl;
+
+    // CGPA calculation (taking previous data)
+    float prevCGPA, prevCredits;
+
+    cout << "\nEnter previous CGPA: ";
+    cin >> prevCGPA;
+
+    cout << "Enter total previous credit hours: ";
+    cin >> prevCredits;
+
+    float cgpa = ((prevCGPA * prevCredits) + totalGradePoints) 
+                 / (prevCredits + totalCredits);
+
+    // Display CGPA
+    cout << "\nFinal CGPA: " << cgpa << endl;
 
     return 0;
 }
